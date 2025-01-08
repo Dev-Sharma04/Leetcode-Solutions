e484923e7cb8edc70f6ac0c134cbb5e148f1b9d8
Dev-Sharma04 <116716973+Dev-Sharma04@users.noreply.github.com>
2025-01-08 22:16:07 +0530

    Update 1408. String Matching in an Array.cpp

--- 1408. String Matching in an Array.cpp
+++ 1408. String Matching in an Array.cpp
@@ -24,3 +24,74 @@ public:
 
 
 //Approach 2: KMP
+//T.c :O(n^2*m)
+//S.C :O(m)
+class Solution {
+public:
+    void computeLPS(string pat, vector<int>&LPS){
+        LPS[0] = 0;
+        int M = pat.size();
+
+        int len = 0;
+        int i = 1;
+        while(i < M){
+            if(pat[i] == pat[len]){
+                len++;
+                LPS[i] = len;
+                i++;
+            }
+            else
+                if(len != 0){
+                    len = LPS[len-1];
+                }
+                else{
+                    LPS[i] = 0;
+                    i++;
+                }
+        }
+    }
+    bool checkKMP(string pat, string txt){
+        int i = 0;
+        int j = 0;
+        int N = txt.size();
+        int M = pat.size();
+
+        vector<int> LPS(M,0);
+        computeLPS(pat,LPS);
+
+        while(i < N){
+            if(txt[i] == pat[j]){
+                i++;
+                j++;
+            }
+            if(j == M){
+                return true;
+            }
+            else{
+                if(i < N && pat[j] != txt[i]){
+                    if(j!= 0){
+                        j = LPS[j-1];
+                    }
+                    else{
+                        i++;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ans;
+        for(int i = 0; i < words.size(); i++){
+            for(int j = 0; j < words.size(); j++){
+                if(i == j)continue;
+
+                if(checkKMP(words[i],words[j])){ //words[i] is pattern words[j] is txt
+                    ans.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
