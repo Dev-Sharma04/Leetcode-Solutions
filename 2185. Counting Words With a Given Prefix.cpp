f3b0c6ed5a25222e258e193b4a00ed8839e89bfb
Dev-Sharma04 <116716973+Dev-Sharma04@users.noreply.github.com>
2025-01-09 08:18:35 +0530

    Update 2185. Counting Words With a Given Prefix.cpp

--- 2185. Counting Words With a Given Prefix.cpp
+++ 2185. Counting Words With a Given Prefix.cpp
@@ -15,3 +15,59 @@ public:
 };
 
 //Approach 2: Using Trie
+//T.C : O(n*l+m)
+//S.C : O(n*l)
+
+struct trieNode{
+    trieNode* children[26];
+    bool isEndOfWord;
+};
+trieNode* getNode(){
+    trieNode* newNode = new trieNode();
+    newNode -> isEndOfWord = false;
+    for(int i = 0; i < 26; i++){
+        newNode -> children[i] = NULL;
+    }
+    return newNode;
+}
+class Trie{
+    public:
+        trieNode* root;
+        Trie(){
+            root = getNode();
+        }
+        void insert(string word){
+            trieNode* crawl = root;
+            for(int i = 0; i < word.size(); i++){
+                int idx = word[i] - 'a';
+                if(crawl -> children[idx] == NULL){
+                    crawl -> children[idx] = getNode();
+                }
+                crawl = crawl -> children[idx];
+            }
+            crawl -> isEndOfWord = true;
+        }
+        bool search(string prefix){
+            trieNode* crawl = root;
+            for(int i = 0; i < prefix.size(); i++){
+                int idx = prefix[i] - 'a';
+                if(crawl -> children[idx] == NULL)return false;
+                crawl = crawl -> children[idx];
+            }
+            return true;
+        }
+
+};
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int cnt = 0;
+        for(int i = 0; i < words.size(); i++){
+            Trie wordTrie;
+
+            wordTrie.insert(words[i]);
+            if(wordTrie.search(pref))cnt++;
+        }
+        return cnt;
+    }
+};
