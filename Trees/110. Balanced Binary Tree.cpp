414b426ac88758a3836ff6140df8cd653b8a36a3
Dev-Sharma04 <116716973+Dev-Sharma04@users.noreply.github.com>
2025-02-15 07:50:59 +0530

    Update 110. Balanced Binary Tree.cpp

--- Trees/110. Balanced Binary Tree.cpp
+++ Trees/110. Balanced Binary Tree.cpp
@@ -1,7 +1,7 @@
 //Question : Check if tree is balanced or not
 
 //Approach : Using recursion
-//T.C : O(n)
+//T.C : O(n^2)
 
 /**
  * Definition for a binary tree node.
@@ -34,5 +34,38 @@ public:
         if(!left || ! right)return false;
         return true;
 
+    }
+};
+
+
+//Approach : Modifying height of binary tree code snippet
+//T.C : O(n)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int check(TreeNode* root){
+        if(!root)return 0;
+
+        int lh = check(root->left);
+        int rh = check(root->right);
+        if(lh == -1 || rh == -1)return -1;
+
+        if(abs(lh-rh) > 1)return -1;
+        return max(lh,rh)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(!root)return true;
+
+        return check(root) != -1;
     }
 };
