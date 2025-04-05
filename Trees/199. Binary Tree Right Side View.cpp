a1c6e7d97903de275652b8c2078a73e388b948c8
Dev-Sharma04 <116716973+Dev-Sharma04@users.noreply.github.com>
2025-04-05 16:21:08 +0530

    Update 199. Binary Tree Right Side View.cpp

--- Trees/199. Binary Tree Right Side View.cpp
+++ Trees/199. Binary Tree Right Side View.cpp
@@ -38,3 +38,82 @@ public:
         return ans;
     }
 };
+
+//Approach : Using BFS (with vector)
+//T.C : O(n)
+//S.C : O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(!root)return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> ans;
+        vector<vector<int>> v;
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> lvl;
+            while(n--){
+                TreeNode* curr = q.front();
+                q.pop();
+                lvl.push_back(curr-> val);
+                if(curr -> left)q.push(curr -> left);
+                if(curr -> right)q.push(curr -> right);
+                
+            }
+            v.push_back(lvl);
+        }
+        for(int i = 0; i < v.size(); i++){
+            ans.push_back(v[i].back());
+        }
+        return ans;
+    }
+};
+
+//Approach : Using DFS 
+//T.C : O(n)
+//S.C : O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void revPreOrder(TreeNode* root, int lvl, vector<int>& ans){
+        if(root == NULL)return;
+        
+        if(ans.size() < lvl)ans.push_back(root->val);
+
+        revPreOrder(root->right,lvl+1,ans);
+        revPreOrder(root->left,lvl+1,ans);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        if(!root) return {};
+        vector<int> ans;
+        revPreOrder(root,1,ans);
+
+        return ans;
+    }
+};
+
+
